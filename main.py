e4ffa98a969ee6e38088112687992239b0c8941d
IbehejD <Ibehej.David@seznam.cz>
2023-04-27 13:56:16 +0200

    finished

--- main.py
+++ main.py
@@ -1,38 +1,37 @@
 import pygame
-from src.solver import *
-from sudoku import Sudoku
+from src.sudokuSolver import SudokuSolver
+from sudoku import Sudoku as SudokuGen
+import numpy as np
 
-# initialise the pygame font
+# inicialisation of font
 pygame.font.init()
-# Title and Icon
-pygame.display.set_caption("SUDOKU SOLVER USING BACKTRACKING")
-# Total window
+# title
+pygame.display.set_caption("SUDOKU SOLVER USING SIMULATED ANNEALING")
 
+# constants
 BORDER_THICK = 7
 THICK_PAD = BORDER_THICK / 2
 DIFF = (500 - BORDER_THICK) / 9
 
-# Load test fonts for future use
+# load fonts 
 FONT1 = pygame.font.SysFont("Arial", 40)
 FONT2 = pygame.font.SysFont("Arial", 20)
 
-# Function to draw required lines for making Sudoku grid
 
+#function drawing grid and numbers
 def draw(grid, def_grid, screen):
-    # Draw the lines
     for i in range(9):
         for j in range(9):
-            if def_grid[i][j] != 0:
-                # Fill blue color in already numbered grid
+            # drawing blue squares over default values
+            if def_grid[i][j]:
                 pygame.draw.rect(screen, (0, 200, 255),
                                  (j * DIFF + THICK_PAD, i * DIFF + THICK_PAD, DIFF + 1, DIFF + 1))
-
-            if grid[i][j] != 0:
-                # Fill grid with default numbers specified
+            #drawing values
+            if grid[i][j]:
                 text1 = FONT1.render(str(grid[i][j]), 1, (0, 0, 0))
                 screen.blit(text1, (j * DIFF + 6 * THICK_PAD, i * DIFF + 2 * THICK_PAD))
 
-    # Draw lines horizontally and verticallyto form grid
+    # drawing lines and frame
     for i in range(10):
         if i % 3 == 0:
             thick = BORDER_THICK
@@ -42,58 +41,75 @@ def draw(grid, def_grid, screen):
         pygame.draw.line(screen, (0, 0, 0), (i * DIFF + THICK_PAD, 0), (i * DIFF + THICK_PAD, 500 - THICK_PAD),
                          thick)
 
-# Display instruction for the game
 
+
+#instruction banner
 def instruction(screen):
-    text1 = FONT2.render("PRESS D TO RESET TO DEFAULT", 1, (0, 0, 0))
-    text2 = FONT2.render("R TO GENERATE NEW SUDOKU", 1, (0, 0, 0))
+    text1 = FONT2.render("PRESS R TO RESET TO DEFAULT", 1, (0, 0, 0))
+    text2 = FONT2.render("N TO GENERATE NEW SUDOKU", 1, (0, 0, 0))
     screen.blit(text1, (20, 520))
     screen.blit(text2, (20, 540))
-
-# Display options when solved
-
+#result banner
 def result(screen):
-    text1 = FONT1.render("FINISHED PRESS R or D", 1, (0, 0, 0))
-    screen.blit(text1, (20, 570))
+    text1 = FONT2.render("FINISHED PRESS N or R", 1, (0, 0, 0))
+    screen.blit(text1, (20, 520))
+#function converting format of generated sudoku
+def generate_new(gen):
+    x = np.array(gen.difficulty(0.5).board)
+    return np.where(x == None, 0, x)
+
 
 if __name__ == "__main__":
-
+    #setting screen
     screen = pygame.display.set_mode((500, 600))
-    sudoku_gen = Sudoku(3)
-    def_grid = generate_new(sudoku_gen)
-    grid = np.copy(def_grid)
+    #inicializing sudoku generator
+    sudoku_gen = SudokuGen(3)
+    #initial grid 
+    initial_grid = generate_new(sudoku_gen)
+    #inicializing sudoku solver
+    sudokuSolver = SudokuSolver(initial_grid)
 
-
+    #run condition variable
     run = True
+    #result condition variable
     rs = 0
-    # The loop thats keep the window running
+
+    # game loop
     while run:
+        #blank screen
         screen.fill((255, 255, 255))
-        # Loop through the events stored in event.get()
+
+        #event handler
         for event in pygame.event.get():
-            # Quit the game window
+            #quiting window
             if event.type == pygame.QUIT:
                 run = False
-            # Get the number to be inserted if key pressed
+            #key handler
             if event.type == pygame.KEYDOWN:
-                # If D is pressed reset the board to default
-                if event.key == pygame.K_d:
-                    rs = 0
-                    grid = np.copy(def_grid)
+                #key R - reset
                 if event.key == pygame.K_r:
-                    def_grid = generate_new(sudoku_gen)
-                    grid = np.copy(def_grid)
-                    rs == 0
+                    rs = 0
+                    sudokuSolver.grid = np.copy(sudokuSolver.def_grid)
+                #key N - new game
+                if event.key == pygame.K_n:
+                    sudokuSolver.def_grid = generate_new(sudoku_gen)
+                    sudokuSolver.grid = np.copy(sudokuSolver.def_grid)
+                    rs = 0
+                #key S - solve
                 if event.key == pygame.K_s:
-                    grid = solveSudoku(grid, def_grid)
-                    rs == 1
+                    sudokuSolver.grid = sudokuSolver.solve()
+                    rs = 1
+        #redraw sudoku
+        draw(sudokuSolver.grid, sudokuSolver.def_grid, screen)
+
+        #banner switch
         if rs == 1:
             result(screen)
+        else:
+            instruction(screen)
 
-        draw(grid, def_grid, screen)
-        instruction(screen)
-        # Update window
+        # update window
         pygame.display.update()
 
-    # Quit pygame window
+    # quit
     pygame.quit()
