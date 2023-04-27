8e5530d4b8178a1218482c2ef25c5281026b73ba
IbehejD <Ibehej.David@seznam.cz>
2023-04-27 18:10:01 +0200

    name change of variable

--- src/solver.py
+++ src/solver.py
@@ -12,154 +12,154 @@ class Solver:
 
     # Cost Function
     def get_errors_number(self, grid):
-        numberOfErrors = 0
+        final_error = 0
         for i in range(0, 9):
-            numberOfErrors += self.get_row_column_errors(i, i, grid)
-        return (numberOfErrors)
+            final_error += self.get_row_column_errors(i, i, grid)
+        return final_error
 
     def get_row_column_errors(self, row, column, grid):
-        numberOfErrors = (
+        row_column_error = (
             9 - len(np.unique(grid[:, column]))) + (9 - len(np.unique(grid[row, :])))
-        return (numberOfErrors)
+        return row_column_error
 
     def get_list_of_blocks(self):
-        finalListOfBlocks = []
+        final_list_of_blocks = []
         for r in range(0, 9):
-            tmpList = []
-            block1 = [i + 3 * ((r) % 3) for i in range(0, 3)]
-            block2 = [i + 3 * math.trunc((r) / 3) for i in range(0, 3)]
-            for x in block1:
-                for y in block2:
-                    tmpList.append([x, y])
-            finalListOfBlocks.append(tmpList)
-        return (finalListOfBlocks)
+            tmp_list = []
+            first_block = [i + 3 * ((r) % 3) for i in range(0, 3)]
+            second_block = [i + 3 * math.trunc((r) / 3) for i in range(0, 3)]
+            for x in first_block:
+                for y in second_block:
+                    tmp_list.append([x, y])
+            final_list_of_blocks.append(tmp_list)
+        return final_list_of_blocks
 
-    def rnd_fill(self, grid, listOfBlocks):
-        for block in listOfBlocks:
+    def rnd_fill(self, grid, list_of_blocks):
+        for block in list_of_blocks:
             for box in block:
                 if grid[box[0], box[1]] == 0:
-                    currentBlock = grid[block[0][0]:(
+                    curr_block = grid[block[0][0]:(
                         block[-1][0] + 1), block[0][1]:(block[-1][1] + 1)]
                     grid[box[0], box[1]] = choice(
-                        [i for i in range(1, 10) if i not in currentBlock])
+                        [i for i in range(1, 10) if i not in curr_block])
         return grid
 
-    def get_block_sum(self, grid, oneBlock):
-        finalSum = 0
-        for box in oneBlock:
-            finalSum += grid[box[0], box[1]]
-        return (finalSum)
+    def get_block_sum(self, grid, block):
+        final_sum = 0
+        for box in block:
+            final_sum += grid[box[0], box[1]]
+        return final_sum
 
     def get_two_rnd_boxes(self,  block):
         while True:
-            firstBox = random.choice(block)
-            secondBox = choice([box for box in block if box is not firstBox])
+            first_box = random.choice(block)
+            second_box = choice([box for box in block if box is not first_box])
 
-            if not (self.is_def(firstBox, )) and not (self.is_def(secondBox, )):
-                return ([firstBox, secondBox])
+            if not (self.is_def(first_box, )) and not (self.is_def(second_box)):
+                return ([first_box, second_box])
 
-    def flip_boxes(self, grid, boxesToFlip):
-        proposedSudoku = np.copy(grid)
-        placeHolder = proposedSudoku[boxesToFlip[0][0], boxesToFlip[0][1]]
-        proposedSudoku[boxesToFlip[0][0], boxesToFlip[0][1]
-                       ] = proposedSudoku[boxesToFlip[1][0], boxesToFlip[1][1]]
-        proposedSudoku[boxesToFlip[1][0], boxesToFlip[1][1]] = placeHolder
-        return (proposedSudoku)
+    def flip_boxes(self, grid, boxes_to_flip):
+        proposed_sudoku = np.copy(grid)
+        place_holder = proposed_sudoku[boxes_to_flip[0][0], boxes_to_flip[0][1]]
+        proposed_sudoku[boxes_to_flip[0][0], boxes_to_flip[0][1]
+                       ] = proposed_sudoku[boxes_to_flip[1][0], boxes_to_flip[1][1]]
+        proposed_sudoku[boxes_to_flip[1][0], boxes_to_flip[1][1]] = place_holder
+        return proposed_sudoku
 
-    def get_proposed_state(self, grid,  listOfBlocks):
+    def get_proposed_state(self, grid,  list_of_blocks):
 
         while True:
-            randomBlock = random.choice(listOfBlocks)
-            if self.number_of_def(randomBlock) < 8:
+            random_block = random.choice(list_of_blocks)
+            if self.number_of_def(random_block) < 8:
                 break
 
-        boxesToFlip = self.get_two_rnd_boxes(randomBlock)
-        proposedSudoku = self.flip_boxes(grid, boxesToFlip)
-        return ([proposedSudoku, boxesToFlip])
+        boxes_to_flip = self.get_two_rnd_boxes(random_block)
+        proposed_sudoku = self.flip_boxes(grid, boxes_to_flip)
+        return ([proposed_sudoku, boxes_to_flip])
 
-    def number_of_def(self, randomBlock):
+    def number_of_def(self, random_block):
         i = 0
-        for x in randomBlock:
+        for x in random_block:
             if self.is_def(x, ):
                 i += 1
         print(i)
         return i
 
-    def is_def(self, indexes, ):
+    def is_def(self, indexes):
         return bool(self.def_grid[indexes[0]][indexes[1]])
 
-    def choose_new_state(self, current_grid,  listOfBlocks, sigma):
-        proposal = self.get_proposed_state(current_grid,  listOfBlocks)
+    def choose_new_state(self, current_grid,  list_of_blocks, sigma):
+        proposal = self.get_proposed_state(current_grid,  list_of_blocks)
         new_grid = proposal[0]
-        boxesToCheck = proposal[1]
-        currentCost = self.get_row_column_errors(boxesToCheck[0][0], boxesToCheck[0][1],
-                                                            current_grid) + self.get_row_column_errors(boxesToCheck[1][0],
-                                                                                                                  boxesToCheck[1][1],
+        boxes_to_check = proposal[1]
+        curr_cost = self.get_row_column_errors(boxes_to_check[0][0], boxes_to_check[0][1],
+                                                            current_grid) + self.get_row_column_errors(boxes_to_check[1][0],
+                                                                                                                  boxes_to_check[1][1],
                                                                                                                   current_grid)
-        newCost = self.get_row_column_errors(boxesToCheck[0][0], boxesToCheck[0][1],
-                                                        new_grid) + self.get_row_column_errors(boxesToCheck[1][0],
-                                                                                                          boxesToCheck[1][1],
+        new_cost = self.get_row_column_errors(boxes_to_check[0][0], boxes_to_check[0][1],
+                                                        new_grid) + self.get_row_column_errors(boxes_to_check[1][0],
+                                                                                                          boxes_to_check[1][1],
                                                                                                           new_grid)
-        costDifference = newCost - currentCost
-        rho = math.exp(-costDifference / sigma)
+        cost_diff = new_cost - curr_cost
+        rho = math.exp(-cost_diff / sigma)
         if (np.random.uniform(1, 0, 1) < rho):
-            return ([new_grid, costDifference])
+            return ([new_grid, cost_diff])
         return ([current_grid, 0])
 
-    def get_itteration_number(self,):
-        numberOfItterations = 0
+    def get_itteration_number(self):
+        itter_number = 0
         for i in range(0, 9):
             for j in range(0, 9):
                 if self.is_def([i, j], ):
-                    numberOfItterations += 1
-        return numberOfItterations
+                    itter_number += 1
+        return itter_number
 
-    def get_initial_sigma(self, grid, listOfBlocks):
-        listOfDifferences = []
+    def get_initial_sigma(self, grid, list_of_blocks):
+        diff_list = []
         tmp_grid = grid
         for i in range(1, 10):
-            tmp_grid = self.get_proposed_state(tmp_grid,  listOfBlocks)[0]
-            listOfDifferences.append(self.get_errors_number(tmp_grid))
-        return (statistics.pstdev(listOfDifferences))
+            tmp_grid = self.get_proposed_state(tmp_grid,  list_of_blocks)[0]
+            diff_list.append(self.get_errors_number(tmp_grid))
+        return (statistics.pstdev(diff_list))
 
     def solve(self):
 
-        solutionFound = 0
-        while (solutionFound == 0):
+        sol_found = 0
+        while (sol_found == 0):
             print("start")
-            decreaseFactor = 0.99
-            stuckCount = 0
-            listOfBlocks = self.get_list_of_blocks()
-            tmp_grid = self.rnd_fill(self.grid, listOfBlocks)
-            sigma = self.get_initial_sigma(self.grid, listOfBlocks)
+            decrease_fact = 0.99
+            stuck_count = 0
+            list_of_blocks = self.get_list_of_blocks()
+            tmp_grid = self.rnd_fill(self.grid, list_of_blocks)
+            sigma = self.get_initial_sigma(self.grid, list_of_blocks)
             score = self.get_errors_number(tmp_grid)
             itterations = self.get_itteration_number()
             if score <= 0:
-                solutionFound = 1
+                sol_found = 1
 
-            while solutionFound == 0:
+            while sol_found == 0:
 
-                previousScore = score
+                prev_score = score
                 for i in range(0, itterations):
-                    newState = self.choose_new_state(
-                        tmp_grid,  listOfBlocks, sigma)
-                    tmp_grid = newState[0]
-                    scoreDiff = newState[1]
-                    score += scoreDiff
+                    new_state = self.choose_new_state(
+                        tmp_grid,  list_of_blocks, sigma)
+                    tmp_grid = new_state[0]
+                    score_diff = new_state[1]
+                    score += score_diff
                     print(score)
                     if score <= 0:
-                        solutionFound = 1
+                        sol_found = 1
                         break
 
-                sigma *= decreaseFactor
+                sigma *= decrease_fact
                 if score <= 0:
-                    solutionFound = 1
+                    sol_found = 1
                     break
-                if score >= previousScore:
-                    stuckCount += 1
+                if score >= prev_score:
+                    stuck_count += 1
                 else:
-                    stuckCount = 0
-                if (stuckCount > 80):
+                    stuck_count = 0
+                if (stuck_count > 80):
                     sigma += 2
                 if (self.get_errors_number(tmp_grid) == 0):
                     break
